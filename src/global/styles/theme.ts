8640baf99787aaaf8130c6a15323683b04241789
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-03-03 00:00:10 -0300

    chore: configure theme colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+	colors: {
+		'gray-300': '#B0B0B0',
+		'gray-500': '#222222',
+		'gray-700': '#1A1A1A',
+
+		'blue-300': '#275FA3',
+		'blue-500': '#2478DF',
+
+		'purple-300': '#5781EF',
+		'purple-500': '#7B61FF',
+
+		red: '#D82D2D',
+		green: '#19940F',
+
+		white: '#FFFFFF',
+		black: '#000000',
+	},
+};
